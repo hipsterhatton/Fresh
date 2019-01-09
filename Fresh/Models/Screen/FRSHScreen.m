b918533c550484290040013ecbd140300c98870a
Stephen Hatton <s.hatton@volcanic.co.uk>
2019-01-09 11:05:35 +0000

    + New model: `Screen`

---
+++ Fresh/Models/Screen/FRSHScreen.m
@@ -0,0 +1,13 @@
+//
+//  FRSHScreen.m
+//  Fresh
+//
+//  Created by Stephen Hatton on 09/01/2019.
+//  Copyright © 2019 Stephen Hatton. All rights reserved.
+//
+
+#import "FRSHScreen.h"
+
+@implementation FRSHScreen
+
+@end
