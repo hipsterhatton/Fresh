b16a43d1238f13a99f94f3257d1d506e9ad6d871
Stephen Hatton <s.hatton@volcanic.co.uk>
2019-01-09 11:29:15 +0000

    + New service: Unsplash API

---
+++ Fresh/Services/Unsplash API/FRSHUnsplashAPI.h
@@ -0,0 +1,13 @@
+//
+//  FRSHUnsplashAPI.h
+//  Fresh
+//
+//  Created by Stephen Hatton on 09/01/2019.
+//  Copyright © 2019 Stephen Hatton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FRSHUnsplashAPI : NSObject
+
+@end
---
+++ Fresh/Services/Unsplash API/FRSHUnsplashAPI.m
@@ -0,0 +1,13 @@
+//
+//  FRSHUnsplashAPI.m
+//  Fresh
+//
+//  Created by Stephen Hatton on 09/01/2019.
+//  Copyright © 2019 Stephen Hatton. All rights reserved.
+//
+
+#import "FRSHUnsplashAPI.h"
+
+@implementation FRSHUnsplashAPI
+
+@end
